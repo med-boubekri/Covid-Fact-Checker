28384199cbfd323a03c4640fbf91534a04758128
Ayoub-2 <ayoubdns28@gmail.com>
2022-01-24 10:54:45 +0100

    update

--- __main__.py
+++ __main__.py
@@ -1,8 +1,8 @@
-from clean import CleanData
+from src.clean import CleanData
 from termcolor import cprint
 if __name__ == "__main__" :
     import timeit
     start = timeit.default_timer()  
-    data_set_cleaned = CleanData("TestDataset.xlsx" , debug=True)
+    data_set_cleaned = CleanData("Dataset/TestDataset.xlsx" , debug=True)
     stop = timeit.default_timer()
     cprint('Time: '+ str(stop - start)  , 'green')  
