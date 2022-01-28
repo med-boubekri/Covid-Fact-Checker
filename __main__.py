21cc602b8f4f34a1cd69368dd34a7fbab3aea678
med-boubekri <Mohammed_boubekri3@um5.ac.ma>
2022-01-28 17:40:06 +0100

    TF-IDF Optimized

--- __main__.py
+++ __main__.py
@@ -11,26 +11,19 @@ if __name__ == "__main__" :
 
     import timeit
     start = timeit.default_timer()
-    inFile = "Dataset/DS_800.xlsx"
-    outFile = "results/Cleaned_DS.csv"
-    
-    if (not(os.path.exists(outFile)) or os.stat(outFile).st_size == 0):
-        train_set_data = CleanData( inFile , debug=True)
-        train_set_data.Dataset.to_csv(outFile, index=False)
-    else:
-        print("File not empty")
-    # print("Dataset is null : ", train_set_data.Dataset.isnull().values.any())
-    
-    DataSet = pd.read_csv(outFile)
-    targets = DataSet.label.values.tolist()
-    print(targets)
-    DataSet.drop(['label'], axis='columns', inplace=True)
-    print(DataSet)
+    inFile = "Dataset/DataSet.xlsx"
+    outFile = "results/Cleaned_DS_test.csv"
+    train_set_data = CleanData( inFile , debug=True)
+    DataSet = train_set_data.Dataset
+    targets = train_set_data.targets
+    #DataSet.drop(['label'], axis='columns', inplace=True)
+    #print(DataSet)
     # cprint("[!] original data : " , 'blue')
     # print(train_set_data.Dataset)
     Train(DataSet , targets)
     stop = timeit.default_timer()
     cprint('Time: '+ str(stop - start) , 'green')
+
     
     
     
